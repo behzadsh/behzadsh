683d53f66ca123658f6fa17234d258a1ec85199f
Behzad Shabani <behzad.shabani@gmail.com>
2015-01-22 22:57:53 +0100

    Add read more section and style it

--- public/themes/default/inc/post.blade.php
+++ public/themes/default/inc/post.blade.php
@@ -3,5 +3,6 @@
 	<div class="date">{{ date("M/d/Y", strtotime($post->publish_date)) }}</div>
 	<div class="content">
 		{{ $post->parsed_intro }}
+	<div class="read-more"><a href="{{ Wardrobe::route('posts.show', $post->slug) }}">ادامه مطلب ←</a></div>
 	</div>
 </div>
