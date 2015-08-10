5d1bbab129d06bcc221aed9ff0e604123b2013b1
Behzad Shabani <behzad.shabani@gmail.com>
2015-08-10 14:19:06 +0430

    Change index view post header to h2

--- public/themes/default/inc/post.blade.php
+++ public/themes/default/inc/post.blade.php
@@ -1,5 +1,5 @@
 <div class="post">
-	<h1><a href="{{ Wardrobe::route('posts.show', $post->slug) }}">{{ $post->title }}</a></h1>
+	<h2><a href="{{ Wardrobe::route('posts.show', $post->slug) }}">{{ $post->title }}</a></h2>
 	<div class="date">{{ date("M/d/Y", strtotime($post->publish_date)) }}</div>
 	<div class="content">
 		{{ $post->parsed_intro }}
