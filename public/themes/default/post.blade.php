7c37a525f92462cc6edac891a01ccf7d33348f60
Behzad Shabani <behzad.shabani@gmail.com>
2015-08-30 12:02:06 +0430

    Fix padding of sharethis block

--- public/themes/default/post.blade.php
+++ public/themes/default/post.blade.php
@@ -13,10 +13,12 @@
 		@include(theme_view('inc.tags'))
 
         <div class="sharethis">
-            <span class='st_facebook_hcount' displayText='Facebook'></span>
-            <span class='st_twitter_hcount' displayText='Tweet'></span>
-            <span class='st_linkedin_hcount' displayText='LinkedIn'></span>
-            <span class='st_email_hcount' displayText='Email'></span>
+            <div class="sharethis-container">
+                <span class='st_facebook_hcount' displayText='Facebook'></span>
+                <span class='st_twitter_hcount' displayText='Tweet'></span>
+                <span class='st_linkedin_hcount' displayText='LinkedIn'></span>
+                <span class='st_email_hcount' displayText='Email'></span>
+            </div>
         </div>
 
         {{ Disqus::comments() }}
